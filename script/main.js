fa83dd5fe9483df362321ba4cb8853f842d2b945
idatewang@tamu.edu <idatewang@tamu.edu>
2022-07-10 14:49:44 -0500

    Password and bgm

--- script/main.js
+++ script/main.js
@@ -26,6 +26,7 @@ const fetchData = () => {
 
 // Animation Timeline
 const animationTimeline = () => {
+  var x = document.getElementById("myAudio").autoplay;
   // Spit chars that needs to be animated individually
   const textBoxChars = document.getElementsByClassName("hbd-chatbox")[0];
   const hbd = document.getElementsByClassName("wish-hbd")[0];
